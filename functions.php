5c73f833fbf9482ed7199f1ae6d604b96aa2cb79
mayi <tala.aguirre@gmail.com>
2024-07-11 12:35:52 +0200

    feat(Header): Ajout du lien admin et des styles

--- functions.php
+++ functions.php
@@ -1,4 +1,8 @@
 <?php
+if (!defined('ABSPATH')) {
+    exit;
+}
+
 // Action qui permet de charger des scripts dans notre thème
 add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
 function theme_enqueue_styles()
@@ -8,3 +12,13 @@ function theme_enqueue_styles()
     // Chargement du fichier avec la personnalisation du css
     wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/css/theme.css', array(), filemtime(get_stylesheet_directory() . '/css/theme.css'));
 }
+
+// Ajout du lien "admin" dans la nav du header lorsque l'utiliateur est connecté
+function add_admin_header_menu_item($items, $args)
+{
+    if (is_user_logged_in() && current_user_can('administrator') && $args->theme_location == 'primary') {
+        $items .= '<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-143"><a href="' . admin_url() . '" class="menu-link" >Admin</a></li>';
+    }
+    return $items;
+}
+add_filter('wp_nav_menu_items', 'add_admin_header_menu_item', 10, 2);
